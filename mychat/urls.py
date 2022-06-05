9e18affa1825522a3594e6b274a56419f1578a22
Balaji <73637765+BalaG-BalaG@users.noreply.github.com>
2022-06-05 16:34:36 +0530

    Update urls.py

--- mychat/urls.py
+++ mychat/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 from django.views.static import serve
-from django.conf.urls import url
+from django.conf.urls import re_path as url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
