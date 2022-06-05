70727f2d238119df4bad08b228a62f3b2abdbd77
Balaji <73637765+BalaG-BalaG@users.noreply.github.com>
2022-06-05 16:40:21 +0530

    Update urls.py

--- mychat/urls.py
+++ mychat/urls.py
@@ -1,13 +1,13 @@
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 from django.views.static import serve
-from django.conf.urls import re_path as url
+#from django.conf.urls import re_path as url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('base.urls')),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
